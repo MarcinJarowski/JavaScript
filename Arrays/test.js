9f27b4147e33cf8c52efaa54c3e1a5ffe87bd5ff
Marcin Jarowski <47415939+MarcinJarowski@users.noreply.github.com>
2019-02-19 17:18:34 +0100

    Add files via upload

---
+++ Arrays/test.js
@@ -0,0 +1,17 @@
+var myArray = [];
+
+myArray[0] = 25;
+myArray[1] = 23;
+myArray[2] = true;
+myArray[3] = "hello";
+
+console.log(myArray);
+
+myArray[2]= false;
+
+var myArray2 = [10,20,"hi",false];
+console.log(myArray2);
+
+console.log(myArray2.length);
+console.log(myArray2.sort());
+console.log(myArray2.reverse());
