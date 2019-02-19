80cc9601c237ae741e996504b1ff1a409fd6bd11
Marcin Jarowski <47415939+MarcinJarowski@users.noreply.github.com>
2019-02-19 12:40:59 +0100

    Add files via upload

---
+++ LogicalOperators/test.js
@@ -0,0 +1,21 @@
+var myAge = 25;
+
+
+
+if (myAge >= 18 && myAge <= 30 ){
+    document.write("You cool dude, you can come");
+}
+
+else{
+    document.write("You can't come");
+}
+
+/*
+if (myAge < 18 || myAge > 30 || myAge===25){
+    document.write("You not coming");
+}
+
+else{
+    document.write("You cool dude, you can come");
+}
+*/
