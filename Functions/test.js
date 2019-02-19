12d43f35359090c493e10aef06189677047f6290
Marcin Jarowski <47415939+MarcinJarowski@users.noreply.github.com>
2019-02-19 15:16:26 +0100

    Add files via upload
    Function which gives average.

---
+++ Functions/test.js
@@ -0,0 +1,19 @@
+/*function getAverage (a,b){
+
+    var average = (a + b) / 2;
+    console.log(average);
+} 
+
+getAverage(7,12);
+
+*/
+
+function getAverage (a,b,c,d,e,f){
+
+    var average = (a + b + c + d + e + f) / 6;
+    console.log(average);
+    return average;
+}
+var myResult = getAverage(7,8,9,10,11,12);
+console.log("The average is " + myResult);
+
