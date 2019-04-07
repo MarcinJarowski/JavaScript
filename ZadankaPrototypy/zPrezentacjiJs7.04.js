a7916e65979e5cbf890510b6b7d5bfda74cac87d
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-04-07 15:42:31 +0200

    added objects made with constructior function

---
+++ ZadankaPrototypy/zPrezentacjiJs7.04.js
@@ -0,0 +1,10 @@
+function Ksiazka(name, year, pages){
+    this.name = name, 
+    this.year = year,
+    this.pages = pages
+}
+
+const ksiazka1 = new Ksiazka("Krysia", "2014", "567");
+console.log(ksiazka1);
+const ksiazka2 = new Ksiazka("Gratulacje", "2093", "123");
+console.log(ksiazka2);
